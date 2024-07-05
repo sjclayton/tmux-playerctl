85f421097d333b0a68671c7ece91c81b2fad8031
Shaun Clayton <shaun.j.clayton@gmail.com>
2024-07-05 11:12:12 -0400

    add logic for multiple players

--- scripts/playerctl_short.sh
+++ scripts/playerctl_short.sh
@@ -12,10 +12,34 @@ short_append_chars_string="@short_append_chars"
 short_max_length="40"
 short_append_chars="..."
 
+#: Define players to poll for metadata
+player1="spotify"
+player2="DeaDBeeF"
+
+#: Check if player is running and retrieve metadata
+check_player() {
+    local player=$1
+    result=$(playerctl -p "$player" metadata --format "{{ artist }} - {{ title }}" 2>&1)
+    
+    if [[ $result == *"No players found"* ]]; then
+        echo "n/a"
+    else
+        echo "$result"
+
+    fi
+}
+
 main() {
   local full shortened info_char_count max_chars append
   max_chars=$(get_tmux_option "$short_max_length_string" "$short_max_length")
-  full=$(playerctl -p spotify metadata --format "{{ artist }} - {{ title }}" | tr '[:upper:]' '[:lower:]')
+
+  full=$(check_player "$player1")
+
+  if [[ "$full" == 'n/a' ]]; then
+  full=$(check_player "$player2")
+  fi
+
+  full=$(echo "$full" | tr '[:upper:]' '[:lower:]')
   shortened=${full:0:$max_chars}
   info_char_count=${#full}
   local append=""
@@ -24,12 +48,7 @@ main() {
     append=$(get_tmux_option "$short_append_chars_string" "$short_append_chars")
   fi
 
-  if ! playerctl -p spotify metadata; then
-  echo "n/a"
-  else
   echo "$shortened$append"
-  fi
-  
 }
 
 main
